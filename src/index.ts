46c649ead3f1943d0d735b395f7684f9eb08f6e7
WMXPY <wm@mengw.io>
2020-08-31 14:13:04 -0500

    add export

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,10 @@
  * @namespace Token
  * @description Index
  */
+
+export * from "./creator";
+export * from "./declare";
+export * from "./generate";
+export * from "./serialize";
+export * from "./verifier";
+
