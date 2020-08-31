c0cea4cc9ef2bef2271590565e29ae09a389989a
WMXPY <wm@mengw.io>
2020-08-31 14:04:13 -0500

    add keypair code

---
+++ src/crypto.ts
@@ -0,0 +1,33 @@
+/**
+ * @author WMXPY
+ * @namespace Token
+ * @description Crypto
+ */
+
+import { createHash, createSign, createVerify, generateKeyPairSync, Hash, Signer, Verify } from 'crypto';
+
+export type KeyPair = {
+
+    readonly public: string;
+    readonly private: string;
+};
+
+export const generateKey = (): KeyPair => {
+
+    const result = generateKeyPairSync('rsa', {
+        modulusLength: 2048,
+        publicKeyEncoding: {
+            type: 'spki',
+            format: 'pem',
+        },
+        privateKeyEncoding: {
+            type: 'pkcs8',
+            format: 'pem',
+        },
+    } as any);
+
+    return {
+        public: result.publicKey,
+        private: result.privateKey,
+    };
+};
