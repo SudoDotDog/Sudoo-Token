9a1c903801373569ebbd72806e85f64fe6bb1685
WMXPY <wm@mengw.io>
2020-09-01 11:48:34 -0500

    add parir convert function

--- src/generate.ts
+++ src/generate.ts
@@ -7,19 +7,29 @@
 import { generateKeyPairSync, KeyPairSyncResult } from 'crypto';
 import { KeyPair } from './declare';
 
+export const convertKeyToSingleLine = (key: string): string => {
+
+    return key.replace(/\n/g, '||');
+};
+
+export const convertKeyToMultiLine = (key: string): string => {
+
+    return key.replace(/\|\|/g, '\n');
+};
+
 export const convertKeyPairToSingleLine = (pair: KeyPair): KeyPair => {
 
     return {
-        public: pair.public.replace(/\n/g, '||'),
-        private: pair.private.replace(/\n/g, '||'),
+        public: convertKeyToSingleLine(pair.public),
+        private: convertKeyToSingleLine(pair.private),
     };
 };
 
 export const convertKeyPairToMultiLine = (pair: KeyPair): KeyPair => {
 
     return {
-        public: pair.public.replace(/\|\|/g, '\n'),
-        private: pair.private.replace(/\|\|/g, '\n'),
+        public: convertKeyToMultiLine(pair.public),
+        private: convertKeyToMultiLine(pair.private),
     };
 };
 
