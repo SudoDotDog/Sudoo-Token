c998bf582aa624fc397acb39f8b39238c19a5a8c
WMXPY <wm@mengw.io>
2020-09-01 11:43:10 -0500

    add keypair convert

--- src/generate.ts
+++ src/generate.ts
@@ -7,6 +7,14 @@
 import { generateKeyPairSync, KeyPairSyncResult } from 'crypto';
 import { KeyPair } from './declare';
 
+export const convertKeyPairToSingleLine = (pair: KeyPair): KeyPair => {
+
+    return {
+        public: pair.public.replace(/\n/g, '||'),
+        private: pair.private.replace(/\n/g, '||'),
+    };
+};
+
 export const generateKeyPair = (): KeyPair => {
 
     const result: KeyPairSyncResult<string, string> = generateKeyPairSync(
