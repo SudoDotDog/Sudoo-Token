33a9e676e53a3d5ab03e99c51a2818f606ad9041
WMXPY <wm@mengw.io>
2020-09-01 11:58:55 -0500

    upgrade attempt convert functions

--- src/generate.ts
+++ src/generate.ts
@@ -12,9 +12,25 @@ export const convertKeyToSingleLine = (key: string): string => {
     return key.replace(/\n/g, '||');
 };
 
+export const attemptConvertKeyToSingleLine = (key: string): string => {
+
+    if (key.includes('\n')) {
+        return convertKeyToSingleLine(key);
+    }
+    return key;
+};
+
 export const convertKeyToMultiLine = (key: string): string => {
 
     return key.replace(/\|\|/g, '\n');
+};
+
+export const attemptConvertKeyToMultiLine = (key: string): string => {
+
+    if (key.includes('||')) {
+        return convertKeyToMultiLine(key);
+    }
+    return key;
 };
 
 export const convertKeyPairToSingleLine = (keyPair: KeyPair): KeyPair => {
@@ -25,11 +41,27 @@ export const convertKeyPairToSingleLine = (keyPair: KeyPair): KeyPair => {
     };
 };
 
+export const attemptConvertKeyPairToSingleLine = (keyPair: KeyPair): KeyPair => {
+
+    return {
+        public: attemptConvertKeyToSingleLine(keyPair.public),
+        private: attemptConvertKeyToSingleLine(keyPair.private),
+    };
+};
+
 export const convertKeyPairToMultiLine = (keyPair: KeyPair): KeyPair => {
 
     return {
         public: convertKeyToMultiLine(keyPair.public),
         private: convertKeyToMultiLine(keyPair.private),
+    };
+};
+
+export const attemptConvertKeyPairToMultiLine = (keyPair: KeyPair): KeyPair => {
+
+    return {
+        public: attemptConvertKeyToMultiLine(keyPair.public),
+        private: attemptConvertKeyToMultiLine(keyPair.private),
     };
 };
 
