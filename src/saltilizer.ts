4ed543adf59257391ab2bfdaba9dbeb49e2e4bbd
WMXPY <wm@mengw.io>
2020-09-07 16:58:56 -0500

    add start end realted

--- src/saltilizer.ts
+++ src/saltilizer.ts
@@ -13,8 +13,64 @@ export class Saltilizer {
 
     private readonly _salt: string;
 
+    private _appendStart: boolean;
+    private _appendEnd: boolean;
+
     private constructor(salt: string) {
 
         this._salt = salt;
+
+        this._appendStart = false;
+        this._appendEnd = true;
+    }
+
+    public enableAppendStart(): this {
+
+        this._appendStart = true;
+        return this;
+    }
+
+    public disableAppendStart(): this {
+
+        this._appendStart = false;
+        return this;
+    }
+
+    public enableAppendEnd(): this {
+
+        this._appendEnd = true;
+        return this;
+    }
+
+    public disableAppendEnd(): this {
+
+        this._appendEnd = false;
+        return this;
+    }
+
+    public encrypt(content: string) {
+
+        const combinedContent: string = content + this._salt;
+    }
+
+    public combine(content: string) {
+
+
+    }
+
+    private getStartContent(): string {
+
+        if (this._appendStart) {
+            return this._salt;
+        }
+        return '';
+    }
+
+    private getEndContent(): string {
+
+        if (this._appendEnd) {
+            return this._salt;
+        }
+        return '';
     }
 }
